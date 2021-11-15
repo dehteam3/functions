16aaebe466e1009eabf9faac07931c0864b6b7b4
dehteam3 <78617156+dehteam3@users.noreply.github.com>
2021-11-15 17:31:03 +0200

    Added print

--- index.ts
+++ index.ts
@@ -14,10 +14,14 @@ async function add(...args: number) {
     }
     return sum
 };
-
-async function random(...args: any[]) {
+async function prin(args: any) {
+    console.log(args)
+}
+async function randomis(...args: any[]) {
     const random = args[Math.floor(Math.random() * args.length)];
     return random
 }
 
-export default { random, add, random }
+export default {
+    random, add, randomis, printt
+}
